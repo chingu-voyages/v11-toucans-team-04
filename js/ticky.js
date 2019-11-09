49063aa2d7e1735df4fc45d9c8e251034c55d299
Nhi Ngo <nhi.nxyn@gmail.com>
2019-11-09 14:50:12 -0500

    Cleaning up code

--- js/ticky.js
+++ js/ticky.js
@@ -1,57 +1,61 @@
-
 // Intersection Observer
 let section = document.querySelector('#how-it-works');
-let observer = new IntersectionObserver((section) => {
-  let tiles = document.querySelectorAll('.feature-tile');
-  if(section[0].intersectionRatio > 0) {
-    tiles.forEach((tile) => {
-      tile.classList.add('feature-visible');
-    });
-  }
+let observer = new IntersectionObserver(section => {
+	let tiles = document.querySelectorAll('.feature-tile');
+	if (section[0].intersectionRatio > 0) {
+		tiles.forEach(tile => {
+			tile.classList.add('feature-visible');
+		});
+	}
 });
 
 observer.observe(section);
 
-
 // Transition Function
 function updateTransition() {
-  let animatedElements = document.querySelectorAll(".animate");
-  for (let i = 0; i < animatedElements.length; i++) {
-    animatedElements[i].className = "reveal-visible";
-  }
+	let animatedElements = document.querySelectorAll('.animate');
+	animatedElements.forEach(animate => {
+		animate.className = 'reveal-visible';
+	});
+	// for (let i = 0; i < animatedElements.length; i++) {
+	//   animatedElements[i].className = "reveal-visible";
+	// }
 }
 
-window.onload = updateTransition();
+window.addEventListener('load', updateTransition);
 
 //Formatting scroll-to-top button
-window.onload = function() {
-  var wrapper = document.getElementById("wrapper");
-  var upScroll = document.getElementById("top-scroll");
+var wrapper = document.getElementById('wrapper');
+var upScroll = document.getElementById('top-scroll');
 
-  wrapper.onscroll = function() {
-    var timer;
-    if (wrapper.scrollTop < 400) {
-      upScroll.style.opacity = "0";
-      timer = setTimeout(function() {
-        if (wrapper.scrollTop < 400) {
-          upScroll.style.visibility = "hidden";
-        }
-      }, 500);
-    } else {
-      upScroll.style.visibility = "visible";
-      upScroll.style.opacity = "1";
-    }
-  };
+function handleTopScroll() {
+	if (wrapper.scrollTop < 400) {
+		upScroll.style.opacity = '0';
+		setTimeout(function() {
+			if (wrapper.scrollTop < 400) {
+				upScroll.style.visibility = 'hidden';
+			}
+		}, 500);
+	} else {
+		upScroll.style.visibility = 'visible';
+		upScroll.style.opacity = '1';
+	}
+}
 
-  // Adding smooth scrolling to anchors
-  var anchors = document.getElementsByClassName("smooth-scroll");
-  Array.prototype.forEach.call(anchors, function(a) {
-    var el = document.getElementById(a.href.match(/#.*$/)[0].substring(1));
-    a.addEventListener("click", function(event) {
-      event.preventDefault();
-      el.scrollIntoView({
-        behavior: "smooth"
-      });
-    });
-  });
-};
+wrapper.addEventListener('scroll', handleTopScroll);
+
+// Adding smooth scrolling to anchors
+function handleSmoothScroll() {
+	var anchors = document.getElementsByClassName('smooth-scroll');
+	Array.prototype.forEach.call(anchors, function(a) {
+		var el = document.getElementById(a.href.match(/#.*$/)[0].substring(1));
+		a.addEventListener('click', function(event) {
+			event.preventDefault();
+			el.scrollIntoView({
+				behavior: 'smooth',
+			});
+		});
+	});
+}
+
+window.addEventListener('load', handleSmoothScroll);
